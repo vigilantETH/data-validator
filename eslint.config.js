d2746cf67466c614b0a7770e3aa2720f894e1bb4
vigilant.eth <olikhovkirill@gmail.com>
2025-06-13 03:36:49 +0300

    add linter and jest

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import stylistics from '@stylistic/eslint-plugin'
+
+export default defineConfig([
+  stylistics.configs.recommended,
+  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.browser } },
+])
