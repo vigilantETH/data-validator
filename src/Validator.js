cd1ba91bfc212550c76f3f73ae0220184841b4d1
vigilant.eth <olikhovkirill@gmail.com>
2025-06-16 13:51:39 +0300

    fix eslint remarks

--- src/Validator.js
+++ src/Validator.js
@@ -1,10 +1,10 @@
+// eslint-disable-next-line class-methods-use-this
 import StringValidator from './validators/StringValidator.js';
 import NumberValidator from './validators/NumberValidator.js';
 import ArrayValidator from './validators/ArrayValidator.js';
 import ObjectValidator from './validators/ObjectValidator.js';
 
 export default class Validator {
-
   string() {
     return new StringValidator();
   }
